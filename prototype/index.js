aa012339f33342ac005c52911be600b0f44f3c76
InteractiveTimmy <interactivetimmy@gmail.com>
2019-04-17 21:51:48 -0400

    added 3rd dimension, converted triangle to box, rendering index buffer, added culling

--- prototype/index.js
+++ prototype/index.js
@@ -2,14 +2,17 @@
 const VTST = [
   'precision mediump float;',
   '',
-  'attribute vec2 vertPosition;', // position 2d
+  'attribute vec3 vertPosition;', // position 2d
   'attribute vec3 vertColor;', // color
   'varying vec3 fragColor;',
+  'uniform mat4 mWorld;',
+  'uniform mat4 mView;',
+  'uniform mat4 mProj;',
   '',
   'void main()',
   '{',
   '  fragColor = vertColor;',
-  '  gl_Position = vec4(vertPosition, 0.0, 1.0);',
+  '  gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);',
   '}',
 ].join('\n');
 
@@ -44,6 +47,18 @@ function init() {
 
   // clear main buffers [color and depth], there are others :)
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // enable depth buffer, this renders based on closeness to view
+  gl.enable(gl.DEPTH_TEST);
+
+  // enable culler, which removes target faces
+  gl.enable(gl.CULL_FACE);
+
+  // tell it which direction is front (gl.CW || gl.CCW)
+  gl.frontFace(gl.CCW)
+
+  // removes face to save render time (gl.FRONT || gl.BACK)
+  gl.cullFace(gl.BACK);
 
   // create shader namespaces
   const vertexShader = gl.createShader(gl.VERTEX_SHADER)
@@ -93,20 +108,86 @@ function init() {
   }
 
   // creat buffers
-  const triangleVerticies = [ // expects x, y, r, g, b
-    0.0, 0.5, 1.0, 0.0, 0.0,
-    -0.5, -0.5, 0.0, 1.0, 0.0,
-    0.5, -0.5, 0.0, 0.0, 1.0,
+  const boxVerticies = [ // expects x, y, r, g, b
+    -1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
+		-1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
+		1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
+		1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
+
+		// Left
+		-1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
+		-1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
+		-1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
+		-1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
+
+		// Right
+		1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
+		1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
+		1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
+		1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
+
+		// Front
+		1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+		1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+
+		// Back
+		1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+		1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+
+		// Bottom
+		-1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
+		-1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
+		1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
+		1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
   ];
 
-  // create object on gpu
-  const triangleVertexBufferObject = gl.createBuffer();
+  const boxIndices = [
+		// Top
+		0, 1, 2,
+		0, 2, 3,
+
+		// Left
+		5, 4, 6,
+		6, 4, 7,
+
+		// Right
+		8, 9, 10,
+		8, 10, 11,
+
+		// Front
+		13, 12, 14,
+		15, 14, 12,
+
+		// Back
+		16, 17, 18,
+		16, 18, 19,
+
+		// Bottom
+		21, 20, 22,
+		22, 20, 23,
+	];
+
+  // create vertex buffer object on gpu
+  const boxVertexBufferObject = gl.createBuffer();
 
   // bind the cpu buffer to gpu buffer (buffertype, bufferobject)
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+  gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
 
   // bind the array to the buffer (buffertype, buffer as typed buffer, render variance)
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVerticies), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVerticies), gl.STATIC_DRAW);
+
+  // create index buffer object on gpu
+  var boxIndexBufferObject = gl.createBuffer();
+
+  // bind the cpu buffer to the gpu buffer (buffertype, bufferobject)
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+
+  // bind the array to the buffer (buffertype, buffer as typed buffer, render variance)
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
 
   // gets attribute from gl program (program => vert, namespace)
   const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition'); // position
@@ -115,10 +196,10 @@ function init() {
   // run position
   gl.vertexAttribPointer(
     positionAttribLocation, // attribute location
-    2, // number of elements per attribute
+    3, // number of elements per attribute
     gl.FLOAT, // type of the elements
     gl.FALSE, // data normalized
-    5 * Float32Array.BYTES_PER_ELEMENT, // size of an individual vertex
+    6 * Float32Array.BYTES_PER_ELEMENT, // size of an individual vertex
     0, // offset from the beginning of a single vertex to this attribute
   );
 
@@ -128,23 +209,80 @@ function init() {
     3, // number of elements per attribute
     gl.FLOAT, // type of the elements
     gl.FALSE, // data normalized
-    5 * Float32Array.BYTES_PER_ELEMENT, // size of an individual vertex
-    2 * Float32Array.BYTES_PER_ELEMENT, // offset from the beginning of a single vertex to this attribute
+    6 * Float32Array.BYTES_PER_ELEMENT, // size of an individual vertex
+    3 * Float32Array.BYTES_PER_ELEMENT, // offset from the beginning of a single vertex to this attribute
   );
 
   // enable
   gl.enableVertexAttribArray(positionAttribLocation);
   gl.enableVertexAttribArray(colorAttribLocation);
 
+  // get uniforms [ constants ]
+  const matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+  const matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+  const matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+
+  // set program to use
+  gl.useProgram(program);
+
+  // create cpu matrix data
+  const worldMatrix = new Float32Array(16);
+  const viewMatrix = new Float32Array(16);
+  const projMatrix = new Float32Array(16);
+
+  // set to identity
+  glMatrix.mat4.identity(worldMatrix); // unchanged, sets to identity
+  glMatrix.mat4.lookAt(viewMatrix, [0, 0, -5], [0, 0, 0], [0, 1, 0]); // view matrix (out: mat4, eye: vec3, center: vec3, up: vec3)
+  glMatrix.mat4.perspective(projMatrix, glMatrix.glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000); // sets to perspective (out: mat4, fov: number, aspect: number, near: number, far: number)
+
+  // send to shader gl[`uniform${type}${size}v`](gpu data, transpose?, cpu data)
+  gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+  gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+  gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
   // main renderer
 
-  function loop() {
-    // set program to use
-    gl.useProgram(program);
+  const xRotationMatrix = new Float32Array(16);
+  const yRotationMatrix = new Float32Array(16);
 
-    // draw the bound buffer (type you are drawing, skip, count)
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+  // generate base matrix
+  let identityMatrix = new Float32Array(16);
+  glMatrix.mat4.identity(identityMatrix);
+
+  // create angle namespace outside of loop
+  let angle;
+
+  function loop() {
+    // get rotation time, one full rotation every 6 seconds that passes
+    angle = performance.now() / 1000 / 6 * 2 * Math.PI;
+
+    // NOTE|OLD: rotate matrix (output: mat4, originalMatrix: mat4, angleToRotate: number, axis: vec3)
+    // NOTE|OLD: glMatrix.mat4.rotate(worldMatrix, identityMatrix, angle, [0, 1, 0]);
+    
+    // rotate around y matrix first
+    glMatrix.mat4.rotate(yRotationMatrix, identityMatrix, angle, [0, 1, 0]);
+
+    // rotate around x matrix second
+    glMatrix.mat4.rotate(xRotationMatrix, identityMatrix, angle / 2, [1, 0, 0]);
+
+    // multiply together to form world transform
+    glMatrix.mat4.mul(worldMatrix, xRotationMatrix, yRotationMatrix);
+
+    // update world matrix
+    gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+
+    // clear screen
+    gl.clearColor(0.2, 0.2, 0.2, 1.0);
+    gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
+
+    // NOTE|OLD: draw the bound buffer (type you are drawing, skip, count)
+    // NOTE|OLD: gl.drawArrays(gl.TRIANGLES, 0, 3);
+    // draw the bound buffer and incidies (type you are drawing, count, type of variable, skip)
+    gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
+
+    requestAnimationFrame(loop);
   }
 
-  loop();
+  // call when screen is ready to draw, does not call when out of focus
+  requestAnimationFrame(loop);
 }
